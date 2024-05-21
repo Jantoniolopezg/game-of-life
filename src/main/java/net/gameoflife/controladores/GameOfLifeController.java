5d6c6e9d552e70742d8b3842e254354e09e46ba9
Jantoniolopezg <157741107+Jantoniolopezg@users.noreply.github.com>
2024-05-22 00:58:59 +0200

    Controlador para la simulacion listo, ahora faltan los demas y el resto de cosas

--- src/main/java/net/gameoflife/controladores/GameOfLifeController.java
+++ src/main/java/net/gameoflife/controladores/GameOfLifeController.java
@@ -1,4 +1,14 @@
 package net.gameoflife.controladores;
 
-public class GameOfLifeController {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class GameOfLifeController extends BaseController{
+
+    @Override
+    public void init() {
+
+    }
 }
--- src/main/java/net/gameoflife/controladores/SimulationController.java
+++ src/main/java/net/gameoflife/controladores/SimulationController.java
@@ -1,4 +1,268 @@
 package net.gameoflife.controladores;
 
-public class SimulationController {
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import net.gameoflife.enumeraciones.TipoIndividuo;
+import net.gameoflife.enumeraciones.TipoRecurso;
+import net.gameoflife.objetos.casilla.Casilla;
+import net.gameoflife.objetos.configuracion.JuegoConfiguracion;
+import net.gameoflife.objetos.individuos.Individuo;
+import net.gameoflife.objetos.recursos.Recurso;
+import net.gameoflife.point.Point2D;
+import net.gameoflife.servicios.CasillaServicio;
+import net.gameoflife.servicios.ConfiguracionServicio;
+import net.gameoflife.servicios.PersistenciaJuegoServicio;
+import net.gameoflife.servicios.SimulacionServicio;
+import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+import static net.gameoflife.Constantes.Constantes.*;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+
+@Controller
+@RequiredArgsConstructor
+public class SimulationController extends BaseController{
+
+    private final CasillaServicio casillaServicio;
+
+    private final SimulacionServicio simulacionServicio;
+
+    private final ConfiguracionServicio configuracionServicio;
+
+    private final PersistenciaJuegoServicio persistenciaJuegoServicio;
+
+    @FXML
+    private ToggleButton activateSimulationButton;
+
+    @FXML
+    private Button stepByStepButton;
+
+    @FXML
+    private ComboBox<String> addElementsCombo;
+
+    @FXML
+    private TextArea cellInfoArea;
+
+    @FXML
+    @Getter
+    private Canvas gameCanvas;
+
+    @FXML
+    @Getter
+    private Label generationLabel;
+
+    @FXML
+    @Getter
+    private Label coordinatesLabel;
+
+    private Timeline timeline;
+
+    private final LongProperty generation = new SimpleLongProperty(0L);
+
+    private final BooleanProperty runningProperty = new SimpleBooleanProperty(false);
+
+
+    @FXML
+    void onSaveGameButtonAction(ActionEvent actionEvent){
+        persistenciaJuegoServicio.save(simulacionServicio.getTablero());
+    }
+
+    @FXML
+    void onRemoveResourcesButtonAction(ActionEvent actionEvent) {
+        final JuegoConfiguracion juegoConfiguracion = configuracionServicio.getConfiguracion().getJuegoConfiguracion();
+        final Casilla[][] tablero = simulacionServicio.getTablero();
+        for (int y = 0; y < juegoConfiguracion.getSize().getY(); ++y) {
+            for (int x = 0; x < juegoConfiguracion.getSize().getX(); ++x) {
+                tablero[x][y].setRecursos(new ArrayList<>());
+            }
+        }
+        drawBoard();
+    }
+
+    @FXML
+    void onRemoveIndividualsButtonAction(ActionEvent actionEvent) {
+        final JuegoConfiguracion juegoConfiguracion = configuracionServicio.getConfiguracion().getJuegoConfiguracion();
+        final Casilla[][] tablero = simulacionServicio.getTablero();
+        for (int y = 0; y < juegoConfiguracion.getSize().getY(); ++y) {
+            for (int x = 0; x < juegoConfiguracion.getSize().getX(); ++x) {
+                tablero[x][y].setIndividuos(new ArrayList<>());
+            }
+        }
+        drawBoard();
+    }
+
+    @FXML
+    void onLoadGameButtonAction(ActionEvent actionEvent){
+        final Casilla[][] tableroCargado = persistenciaJuegoServicio.load();
+        if (tableroCargado != null){
+            simulacionServicio.setTablero(tableroCargado);
+            drawBoard();
+        }
+    }
+
+    @FXML
+    void onResetButtonAction(ActionEvent actionEvent) {
+        final JuegoConfiguracion juegoConfiguracion = configuracionServicio.getConfiguracion().getJuegoConfiguracion();
+        final Casilla[][] tablero = simulacionServicio.getTablero();
+        for (int y = 0; y < juegoConfiguracion.getSize().getY(); ++y) {
+            for (int x = 0; x < juegoConfiguracion.getSize().getX(); ++x) {
+                tablero[x][y] = Casilla.builder().recursos(new ArrayList<>()).individuos(new ArrayList<>()).build();
+            }
+        }
+        drawBoard();
+    }
+
+    @FXML
+    void onStepByStepButtonAction(ActionEvent actionEvent){
+        runGameLoop();
+    }
+    @FXML
+    @Override
+    public void init() {
+        initTimeLine();
+        clearBoard(configuracionServicio.getConfiguracion().getJuegoConfiguracion().getSize());
+        initCombo();
+        initActivateSimulationButton();
+        generationLabel.textProperty().bind(generation.asString());
+    }
+
+    @FXML
+    void onMouseClickedCanvas(MouseEvent event){
+        final String mouseButtonName = event.getButton().name();
+        final boolean isPrimary = PRIMARY.equals(mouseButtonName);
+        final boolean isSecondary = SECONDARY.equals(mouseButtonName);
+        final Point2D<Double> mouseCoordinates = new Point2D<>(event.getX(),event.getY());
+        final Point2D<Double> canvasSize = new Point2D<>(gameCanvas.getWidth(), gameCanvas.getHeight());
+        casillaServicio.getCasillaDePosicionEnPantalla(simulacionServicio.getTablero(), mouseCoordinates, canvasSize).ifPresent((casilla) ->{
+            if (isPrimary){
+                simulacionServicio.addElemento(generation.getValue(), casilla, addElementsCombo.getSelectionModel().getSelectedItem());
+                drawBoard();
+            } else if (isSecondary) {
+                simulacionServicio.removeElemento(generation.getValue(), casilla, addElementsCombo.getSelectionModel().getSelectedItem());
+                drawBoard();
+            }
+        });
+    }
+
+    @FXML
+    void onMouseMoved(MouseEvent event){
+        final Point2D<Double> coordenadasRaton = new Point2D<>((double)event.getX(), (double)event.getY());
+        if ((coordenadasRaton.getX() >= 0 && coordenadasRaton.getX() <= gameCanvas.getWidth() - 1) && (coordenadasRaton.getY() >= 0 && coordenadasRaton.getY() <= gameCanvas.getHeight() - 1)) {
+            final int redondeadoX = (int) Math.floor(coordenadasRaton.getX() / CELL_WIDTH);
+            final int redondeadoY = (int) Math.floor(coordenadasRaton.getY() / CELL_HEIGHT);
+            coordinatesLabel.setText("X: " + redondeadoX + " Y: " + redondeadoY);
+        }else{
+            coordinatesLabel.setText(EMPTY);
+        }
+        final Point2D<Double> canvasSize = new Point2D<>(gameCanvas.getWidth(), gameCanvas.getHeight());
+        casillaServicio.getCasillaDePosicionEnPantalla(simulacionServicio.getTablero(), coordenadasRaton, canvasSize)
+                .ifPresentOrElse(casilla -> cellInfoArea.setText(casilla.toString()), () -> cellInfoArea.setText(EMPTY));
+    }
+
+    @FXML
+    public void onFinishButtonAction(ActionEvent actionEvent){
+        timeline.stop();
+    }
+
+    /**
+     * Dibuja el tablero en el gameCanvas.
+     */
+    public void drawBoard() {
+        final Casilla[][] tablero = simulacionServicio.getTablero();
+        final Point2D<Integer> tableroSize = configuracionServicio.getConfiguracion().getJuegoConfiguracion().getSize();
+        clearBoard(tableroSize);
+        final GraphicsContext graphicsContext2D = gameCanvas.getGraphicsContext2D();
+        graphicsContext2D.setFill(Color.LIGHTBLUE);
+
+        for (int y = 0; y < tableroSize.getY(); y++) {
+            for (int x = 0; x < tableroSize.getX(); x++){
+                final Casilla casilla = tablero[x][y];
+                graphicsContext2D.fillRect((x * CELL_WIDTH) + 1, (y * CELL_HEIGHT) + 1, CELL_WIDTH - 1, CELL_HEIGHT - 1);
+
+                final List<Individuo> individuos = casilla.getIndividuos();
+                for (int individuoIndex = 0; individuoIndex < individuos.size(); individuoIndex++) {
+                    final Image image = new Image(Objects.requireNonNull(getClass().getResource(individuos.get(individuoIndex).getTipoIndividuo().getImageResourceName()).toExternalForm()));
+                    graphicsContext2D.drawImage(image, (x * CELL_WIDTH) + individuoIndex * IMAGE_WIDTH, (y * CELL_HEIGHT));
+                }
+                final List<Recurso> recursos = casilla.getRecursos();
+                for (int recursoIndex = 0; recursoIndex < recursos.size(); recursoIndex++) {
+                    final Image image = new Image(Objects.requireNonNull(getClass().getResource(recursos.get(recursoIndex).getTipoRecurso().getImageResourceName()).toExternalForm()));
+                    graphicsContext2D.drawImage(image, (x * CELL_WIDTH) + recursoIndex * IMAGE_WIDTH, (y * CELL_HEIGHT) + IMAGE_HEIGHT);
+                }
+            }
+        }
+    }
+
+    private void initTimeLine() {
+        final KeyFrame keyFrame = new KeyFrame(Duration.millis(SIMULATION_SPEED), actionEvent -> {
+            runGameLoop();
+        });
+        this.timeline = new Timeline(keyFrame);
+        timeline.setCycleCount(Animation.INDEFINITE);
+        runningProperty.addListener((observable, oldValue, newValue) -> {
+            if (newValue){
+                timeline.play();
+            }else {
+                timeline.stop();
+            }
+        });
+    }
+
+    private void clearBoard(Point2D<Integer> size) {
+        gameCanvas.setWidth(size.getX() * CELL_WIDTH);
+        gameCanvas.setHeight(size.getY() * CELL_HEIGHT);
+        final GraphicsContext graphicsContext2D = gameCanvas.getGraphicsContext2D();
+        graphicsContext2D.setFill(Color.WHITE);
+        graphicsContext2D.fillRect(0, 0, gameCanvas.getWidth(), gameCanvas.getHeight());
+    }
+
+    private void initCombo() {
+        Arrays.stream(TipoIndividuo.values())
+                .filter(tipoIndividuo -> tipoIndividuo != TipoIndividuo.NINGUNO)
+                .forEach(tipoIndividuo -> addElementsCombo.getItems().add(INDIVIDUAL + tipoIndividuo.name()));
+        Arrays.stream(TipoRecurso.values())
+                .filter(tipoRecurso -> tipoRecurso != TipoRecurso.VACIO)
+                .forEach(tipoRecurso -> addElementsCombo.getItems().add(RESOURCE + tipoRecurso.name()));
+        addElementsCombo.disableProperty().bind(runningProperty);
+        stepByStepButton.disableProperty().bind(runningProperty);
+    }
+
+    private void initActivateSimulationButton() {
+        activateSimulationButton.setOnAction((actionEvent) -> runningProperty.set(!runningProperty.get()));
+        activateSimulationButton.textProperty().bind(
+                Bindings.when(runningProperty)
+                        .then("Parar simulacion")
+                        .otherwise("Activar simulacion"));
+    }
+
+    /**
+     * Este es el bucle principal del juego, manejado por el Timeline.
+     */
+    private void runGameLoop() {
+        simulacionServicio.runGameLoop(generation.get());
+        Platform.runLater(this::drawBoard);
+        generation.set(generation.get() + 1);
+    }
 }
